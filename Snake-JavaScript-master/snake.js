caa02a2269901e571c22875a14c00f5a99453cd5
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-17 23:59:51 +0200

    Creating unit+Images+Audio

--- Snake-JavaScript-master/snake.js
+++ Snake-JavaScript-master/snake.js
@@ -1,3 +1,30 @@
 // Sam's Production
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
+
+// create the unit
+const box = 32;
+
+// load images
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// load audio files
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";

8ef0657908feb32c4156dbaa72acbab20bf9fbe3
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-18 00:54:16 +0200

    TestingCanvasOnBrowserAfterMakingDrawFunctBy50ms

--- Snake-JavaScript-master/snake.js
+++ Snake-JavaScript-master/snake.js
@@ -1,17 +1,50 @@
 // Sam's Production
+
+// Creating Canvas
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
-
 // create the unit
 const box = 32;
 
 // load images
-
 const ground = new Image();
 ground.src = "img/ground.png";
 
 const foodImg = new Image();
 foodImg.src = "img/food.png";
+// Creating the snake
+
+let snake = [];
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+}
+
+// Create the food ( Object )
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// Creating the score var
+
+let score = 0;
+
+// Draw everything to our convas
+
+function draw(){
+
+    ctx.drawImage(ground,0,0);
+}
+
+// Call draw function every 50 ms
+
+let game = setInterval(draw,50);
+
+
+
+
 
 // load audio files
 
